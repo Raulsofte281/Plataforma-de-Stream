d1fafebd923031e1885ebfac6ea67422c01de064
Raul Machado <rauldondonimachado09@gmail.com>
2024-08-31 21:16:34 -0300

    Adicionando insider

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,36 @@
 import Background from '@/assets/Background.svg'
 import bell from '@/assets/bell.svg'
 import search from '@/assets/search.svg'
+import FunArts from '@/assets/FunxArts.jpeg'
 
 
 
 export function App(){
     return (
         <>
-    <div className="bg-[url('./assets/Background.svg')]">
+    <div className="bg-[url('./assets/Background.svg')] h-full w-full p-2">
         <div>
             <ul className="flex justify-between text-base  font-[Poppins] text-white">
                 
-                <ul className='flex gap-5 font-medium'>
-                    <li>Movies</li>
+                <ul className='flex gap-5 text-wrap font-medium p-1'>
+                    <li className=''>Movies</li>
                     <li>Series</li>
                     <li>Documentaries</li>
                 </ul>
-
-                    <ul className='flex'>
-                        <img src={search}/>
-                        <img src={bell} />
+                
+                    <ul className='flex gap-3'>
+                        <img  src={search}/>
+                        <img  src={bell} />
+                        <li className='flex items-center gap-1'><img className='rounded-full size-8 ' src={FunArts}/>Warrior</li>
                     </ul>
             </ul>
-
-            
         </div>
+        <p className='text-white text-5xl font-[Poppins] font-medium'>Insider</p>
+        <p className='font-[Poppins] text-white text-sm font-light'>2022 | Comedy horror | 1 Season</p>
+    </div>
+
+    <div className='bg-[#303030] w-full h-full'>
+a
     </div>
         </>
     );
