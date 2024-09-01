752b7d779afe1275917444bfb2ee609939571745
Raul Machado <rauldondonimachado09@gmail.com>
2024-09-01 19:59:01 -0300

    Colocando botoes e icones

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 /* assets */
-import Background from '@/assets/Background.svg'
-import bell from '@/assets/bell.svg'
-import search from '@/assets/search.svg'
-import FunArts from '@/assets/FunxArts.jpeg'
-
+import Background from '@/assets/Background.svg';
+import bell from '@/assets/bell.svg';
+import search from '@/assets/search.svg';
+import FunArts from '@/assets/FunxArts.jpeg';
+import Heart from '@/assets/Heart.svg';
+/* Componetes */
+import { Button } from '@/components/ui/button'
 
 
 export function App(){
@@ -19,15 +21,29 @@ export function App(){
                     <li>Documentaries</li>
                 </ul>
                 
-                    <ul className='flex gap-3'>
-                        <img  src={search}/>
+                    <ul className='flex gap-3 items-center'>
+                        <img  className='size-6' src={search}/>
                         <img  src={bell} />
                         <li className='flex items-center gap-1'><img className='rounded-full size-8 ' src={FunArts}/>Warrior</li>
                     </ul>
             </ul>
         </div>
-        <p className='text-white text-5xl font-[Poppins] font-medium'>Insider</p>
-        <p className='font-[Poppins] text-white text-sm font-light'>2022 | Comedy horror | 1 Season</p>
+        {/* Botoes, insider, e paragrafos */}
+        <div className='mt-20'>
+            <p className='text-white text-5xl font-[Poppins] font-medium'>Insider</p>
+            <p className='font-[Poppins] text-white text-sm font-light  mt-4'>2022 | Comedy horror | 1 Season</p>
+            {/* div englobando os botoes */}
+            <div className='flex gap-3 mt-4 items-center'>
+                <Button
+                variant={'default'}
+                className='rounded-xl p-6 text-base font-medium text-white bg-purple-700'>Watch now
+                </Button>
+                <Button 
+                variant={'outline'} 
+                className='rounded-2xl size-[50px] bg-gradient-to-r from-[#FFFFFF] to-gray-300'><img src={Heart} className='size-full '/></Button>
+            </div>
+        </div>
+        
     </div>
 
     <div className='bg-[#303030] w-full h-full'>
