27568261cb370226670b616327eb691ee7dca41b
Raul Machado <rauldondonimachado09@gmail.com>
2024-09-08 19:08:00 -0300

    Adicionando Painel de Stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Button } from "./components/ui/button";
 import { Card } from "./components/ui/card";
 
+/* assets */
 import Background from '@/assets/Background.svg'
 import bell from '@/assets/bell.svg'
 import Heart from '@/assets/Heart.svg'
 import search from '@/assets/search.svg'
 import FunxArts from '@/assets/FunxArts.jpeg'
 import kimetsu from '@/assets/kimetsu.svg'
+import bleach from '@/assets/bleach.svg'
+import Blackcover from '@/assets/Blackcover.svg'
 
 function App() {
   return (
@@ -46,9 +49,22 @@ function App() {
         
     </div>
 
-    <div className='bg-[#303030] w-full h-full font-[Poppins] p-2 pb-20'>
-    <h1 className="text-white font-semibold text-xl">Trending</h1>
-    <Card className="bg-[url('./assets/kimetsu.svg')] bg-cover w-full bg-[url('./assets/kimetsu.svg')] h-full flex justify-end"/>
+    <div className='bg-[#303030] w-full h-full font-[Poppins] p-2 pb-20 max-sm:pb-28'>
+    <h1 className="text-white font-semibold text-xl flex justify-center">Trending</h1>
+        <div className="flex justify-center gap-16 flex-wrap mt-8 ">
+            <Card className="bg-[url('./assets/kimetsu.svg')] bg-cover w-full  h-full flex justify-end" tittle='Demon slayer' SubTittle="2019 || Adventure"/>
+            <Card className="bg-[url('https://i.pinimg.com/originals/a3/cd/37/a3cd3714d39c9585f944676969548cd4.jpg')]
+            bg-cover rounded-xl max-sm:mt-10 w-full  h-full flex justify-end"
+            tittle='Jujutsu Kaisen'
+            SubTittle='2018 || dark fantasy'/>
+            <Card className="bg-[url('./assets/bleach.svg')] bg-cover w-full max-sm:mt-20  h-full flex justify-end"
+            tittle='Bleach'
+            SubTittle='2011 || Fantasy'/>
+            <Card className="bg-[url('./assets/Blackcover.svg')] bg-cover w-full max-sm:mt-20   h-full flex justify-end"
+            tittle='Black cover'
+            SubTittle='2015 || Magic'
+            />
+        </div>
     </div>
     </>
   );
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,19 +7,19 @@ import Heart from '@/assets/Heart.svg'
 
 /* bg-[url('./assets/kimetsu.svg')] bg-cover w-full bg-[url('./assets/kimetsu.svg')] h-full flex justify-end*/
 
-export function Card({className = ''})  {
+export function Card({className = '', tittle, SubTittle}: Props)  {
   return (
     <div className="w-[255px] h-[321px]">
       <div className={`${className}`}>
               <Button 
                   variant={'outline'} 
-                  className='rounded-2xl m-2 size-[50px] bg-gradient-to-r from-[#FFFFFF] to-gray-300'><img src={Heart} className='size-full'/>
+                  className='rounded-2xl m-2 size-[50px] bg-gradient-to-r from-[#FFFFFF] to-gray-300 '><img src={Heart} className='size-full'/>
               </Button>
               
       </div>
           <div className="bg-gradient-to-r from-[#FFFFFF] to-gray-300 w-full p-3 ">
-              <h1 className="font-semibold text-base">Demon slayer</h1>
-              <p className="font-normal text-sm">2019 | Adventure</p>
+              <h1 className="font-semibold text-base">{tittle}</h1>
+              <p className="font-normal text-sm">{SubTittle}</p>
           </div> 
      
       
